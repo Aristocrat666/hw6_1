ec6fbe6ddda8fd94b913b26b3c55776109afd748
levoschkin <levoschkin@IGORILIN>
2020-02-17 12:54:37 +0300

    Initial commit

---
+++ src/Server/Server.java
@@ -0,0 +1,29 @@
+package Server;
+
+import java.io.IOException;
+import java.net.*;
+public class Server {
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Подключение к серверу");
+            while (true) {
+                socket = server.accept();
+                System.out.println("Подключение клиента");
+                new Thread(new ClientHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
